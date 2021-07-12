313802e3370186121991d310062f7193e2645f21
Антон Барабошин <baraboshin@neolant.com>
2021-07-13 01:47:17 +0300

    1) Накидал предварительный каркас.2)Добавил шрифты. 3)Подключил bootstrap.

---
+++ js/timer.js
@@ -0,0 +1,22 @@
+const time = {
+	seconds: 0,
+	minutes: 0,
+	hours: 0,
+};
+
+
+function timeCounter() {
+	time.seconds += 1;
+	if(time.seconds === 60) {
+		time.minutes += 1;
+		time.seconds = 0;
+	}
+	if(time.minutes === 60) {
+		time.hours += 1;
+		time.minutes = 0;
+	}
+	let el = document.querySelector(".timerValue");
+	el.textContent = time.hours.toString().padStart(2,"0") + ":" + time.minutes.toString().padStart(2,"0") + ":" + time.seconds.toString().padStart(2,"0");
+};
+setInterval(() => timeCounter(), 1000);
+
